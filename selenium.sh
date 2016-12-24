c83387bb2f7923eb4fa73689a766a9fa7d75af9a
Sergii Bondarenko <broken@firstvector.org>
2016-12-24 17:43:17 +0400

    Init commit

---
+++ selenium.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+DOWNLOAD_URL="$1"
+FILENAME="selenium.jar"
+ROOT=".vagrant"
+
+if [[ -z ${DOWNLOAD_URL} || ! -d ${ROOT} ]]; then
+  echo "Download URL or ${ROOT} directory does not exists."
+  exit 1
+fi
+
+DESTINATION="${ROOT}/${FILENAME}"
+LOGFILE="${ROOT}/${FILENAME%%.*}"
+PIDFILE="${LOGFILE}.pid"
+
+if [ ! -f ${DESTINATION} ]; then
+  curl -O ${DOWNLOAD_URL}
+
+  if [ $? -gt 0 ]; then
+    echo "Cannot download file: ${DOWNLOAD_URL}"
+    exit 1
+  fi
+
+  mv ${DOWNLOAD_URL##*/} ${DESTINATION}
+fi
+
+if [ -f ${PIDFILE} ]; then
+  PID=$(cat ${PIDFILE})
+
+  if [ -n ${PID} ]; then
+    kill ${PID} > /dev/null 2>&1
+  fi
+fi
+
+nohup java -jar ${DESTINATION} -role node > ${LOGFILE}.out.log 2> ${LOGFILE}.error.log < /dev/null &
+echo $! > ${PIDFILE}
---
+++ tests/behat/features/bootstrap/FeatureContext.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * @file
+ * Feature context.
+ */
+
+// Contexts.
+use Behat\MinkExtension\Context\RawMinkContext;
+
+// Helpers.
+use Behat\Gherkin\Node\PyStringNode;
+use Behat\Gherkin\Node\TableNode;
+
+/**
+ * Class FeatureContext.
+ */
+class FeatureContext extends RawMinkContext {
+
+}
